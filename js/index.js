ed69bf9bf818f3c198c35ddf57cad95ee3d8c417
Bilal Azahaf <bilal.azahaf@student.ehb.be>
2023-04-26 10:24:54 +0200

    map and marker op school

--- js/index.js
+++ js/index.js
@@ -8,21 +8,29 @@ const app = {
     map: null, // gebruik dit om de map gemakkelijk aan te spreken doorheen de applicatie
     init() {
         // initialise de kaart
+        var map = L.map('map').setView([50.8459034, 4.3580679], 13);
 
         // voeg een tile layer toe, met URL https://a.tile.openstreetmap.org/{z}/{x}/{y}.png
         // vergeet openstreetmap attributie niet
-        
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
         // gebruik de functie "loadMarkers" om de markers toe te voegen
+        L.marker([50.842239, 4.322808]).addTo(map)
+        .bindPopup('Campus Kaai is de thuis van onze studenten Design & Technologie, Musical en RITCS.')
+        .openPopup();
     },
     loadMarkers() {
 
         // fetch de data van opendata.brussels.be
-            // als er coordinaten beschikbaar zijn, kan je de addMarker functie gebruiken om een marker toe te voegen op de kaart
-        
-        
+        // als er coordinaten beschikbaar zijn, kan je de addMarker functie gebruiken om een marker toe te voegen op de kaart
+
+
     },
     addMarker(lat, lon) {
         // voeg een marker toe op lat, lon
+
     }
 }
 
